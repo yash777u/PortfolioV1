39c15e37373ec6798905f1d9f6fa1eb8ee30e1d0
yash777u <yogeshkumardagar7@gmail.com>
2024-06-27 21:45:57 +0530

    hero learn more button

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -63,7 +63,7 @@ const HeroContent = () => {
         <motion.a
           href="#about-me"
           variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] mx-auto md:mx-0"
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] mx-auto md:mx-0 p-2"
         >
           Learn More!
         </motion.a>
