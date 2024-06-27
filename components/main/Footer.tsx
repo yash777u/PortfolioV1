6b6f4ab480a706886d45b8504291a81d48b46ea6
yash777u <yogeshkumardagar7@gmail.com>
2024-06-27 21:39:27 +0530

    navigation backdrop blurr

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -4,7 +4,7 @@ import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] z-30 ">
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
         <div className="w-full flex flex-col items-center justify-center m-auto">
             <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
                 
@@ -23,7 +23,7 @@ const Footer = () => {
             </div>
 
             <div className="mb-[20px] text-[15px] text-center">
-                &copy; WebChain Dev 2023 Inc. All rights reserved
+                &copy; Yogesh Kumar @ 2024. All rights reserved
             </div>
         </div>
     </div>
